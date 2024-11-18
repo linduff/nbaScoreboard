426f304e0567d6d4403af5830f4f1b983eb4355d
linduff <calinduff@gmail.com>
2024-11-18 15:30:54 -0500

    changed json to dict

--- nbaScoreboard.py
+++ nbaScoreboard.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from nba_api.live.nba.endpoints import scoreboard
 
-games = scoreboard.ScoreBoard().get_json()
+games = scoreboard.ScoreBoard().get_dict()
 print(games)
 
 app = Flask(__name__)
