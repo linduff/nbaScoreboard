9f3ba2900f354e47fa1d4e63834435bc060f51eb
linduff <calinduff@gmail.com>
2024-11-18 14:08:08 -0500

    Added nba_api

--- nbaScoreboard.py
+++ nbaScoreboard.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from nba_api.live.nba.endpoints import scoreboard
+
+games = scoreboard.ScoreBoard().get_json()
 
 app = Flask(__name__)
 
 @app.route('/')
 def scoreboard():
-    return render_template('scoreboard.html')
+    return render_template('scoreboard.html', games = games)
