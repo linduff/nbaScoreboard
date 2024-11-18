5c2f35b6b8a5572ec1dc88cdc8d453b97d3c106d
Caleb Linduff <calinduff@gmail.com>
2024-11-18 14:47:37 -0500

    Added scores

--- nbaScoreboard.py
+++ nbaScoreboard.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 from nba_api.live.nba.endpoints import scoreboard
 
 games = scoreboard.ScoreBoard().get_json()
+print(games)
 
 app = Flask(__name__)
 
 @app.route('/')
 def scoreboard():
-    return render_template('scoreboard.html', games = games)
+    return render_template('scoreboard.html', games = games["scoreboard"]["games"])
