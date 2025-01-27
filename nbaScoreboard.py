920487d051841e4ee72df194019142545ac278d4
Caleb Linduff <calinduff@gmail.com>
2025-01-27 09:45:20 -0500

    moved games into call

--- nbaScoreboard.py
+++ nbaScoreboard.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from nba_api.live.nba.endpoints import scoreboard
 
-games = scoreboard.ScoreBoard().get_dict()
-print(games)
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def scoreboard():
+def home():
+    games = scoreboard.ScoreBoard().get_dict()
     return render_template('scoreboard.html', games = games["scoreboard"]["games"])
 
 app.run(host="0.0.0.0")
